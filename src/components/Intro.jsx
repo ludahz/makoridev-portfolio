a2ab1023112afe624e90d8e722e6ea2457151934
ludahz <ludahmaxs@gmail.com>
2022-02-13 17:48:57 +0300

    modified links

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,47 +4,55 @@ import { IoIosArrowDown } from 'react-icons/io'
 import Link from 'next/link'
 
 import {
-  Section,
-  SectionHeader,
-  SectionSmallHeader,
-  SocialIcons,
-  SocialLinks,
-  Span,
-  LeftSection,
-  RightSection,
-  Arrow,
+	Section,
+	SectionHeader,
+	SectionSmallHeader,
+	SocialIcons,
+	SocialLinks,
+	Span,
+	LeftSection,
+	RightSection,
+	Arrow,
 } from '../styles/components/intro'
 
 const Intro = () => {
-  return (
-    <Section id='home'>
-      <LeftSection>
-        <SectionSmallHeader>
-          Hi, I{"'"}m <Span>Makori</Span>
-        </SectionSmallHeader>
-        <SectionHeader>Web Developer</SectionHeader>
-        <SocialLinks>
-          <SocialIcons href='https://github.com/ludahz'>
-            <AiFillGithub size='1.8rem' />
-          </SocialIcons>
+	return (
+		<Section id='home'>
+			<LeftSection>
+				<SectionSmallHeader>
+					Hi, I{"'"}m <Span>Makori</Span>
+				</SectionSmallHeader>
+				<SectionHeader>Web Developer</SectionHeader>
+				<SocialLinks>
+					<SocialIcons
+						href='https://github.com/ludahz'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<AiFillGithub size='1.8rem' />
+					</SocialIcons>
 
-          <SocialIcons href='https://www.linkedin.com/in/lawrence-makori-230bb348/'>
-            <AiFillLinkedin size='1.8rem' />
-          </SocialIcons>
-        </SocialLinks>
-      </LeftSection>
-      <RightSection>
-        <BackgroundAnimation />
-      </RightSection>
-      <Arrow>
-        <Link href='/#about'>
-          <a>
-            <IoIosArrowDown size='4rem' />
-          </a>
-        </Link>
-      </Arrow>
-    </Section>
-  )
+					<SocialIcons
+						href='https://www.linkedin.com/in/lawrence-makori-230bb348/'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<AiFillLinkedin size='1.8rem' />
+					</SocialIcons>
+				</SocialLinks>
+			</LeftSection>
+			<RightSection>
+				<BackgroundAnimation />
+			</RightSection>
+			<Arrow>
+				<Link href='/#about'>
+					<a>
+						<IoIosArrowDown size='4rem' />
+					</a>
+				</Link>
+			</Arrow>
+		</Section>
+	)
 }
 
 export default Intro
