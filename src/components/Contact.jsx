2f055c83771ec49fb1fd5756bdc2352e605454c0
ludahz <ludahmaxs@gmail.com>
2022-02-13 17:26:54 +0300

    updated dependecies and modified project links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,36 +1,36 @@
 import {
-  Section,
-  SectionTitle,
-  Title,
-  ContentTitle,
-  Margin,
+	Section,
+	SectionTitle,
+	Title,
+	ContentTitle,
+	Margin,
 } from '../styles/globalSections'
 
 import {
-  Container,
-  LeftSection,
-  LeftContainer,
-  InfoContainer,
-  TopDiv,
-  MailDiv,
-  InfoDiv,
-  Info,
-  Span,
-  RightSection,
-  RightContainer,
-  SocialDiv,
-  SocialIcon,
-  SocialSection,
-  Line1,
-  Line2,
-  ContactContainer,
-  ContentDiv,
-  Form,
-  Label,
-  LabelDiv,
-  TextArea,
-  Button,
-  Input,
+	Container,
+	LeftSection,
+	LeftContainer,
+	InfoContainer,
+	TopDiv,
+	MailDiv,
+	InfoDiv,
+	Info,
+	Span,
+	RightSection,
+	RightContainer,
+	SocialDiv,
+	SocialIcon,
+	SocialSection,
+	Line1,
+	Line2,
+	ContactContainer,
+	ContentDiv,
+	Form,
+	Label,
+	LabelDiv,
+	TextArea,
+	Button,
+	Input,
 } from '../styles/components/contact'
 
 import { AiOutlineMail, AiFillLinkedin } from 'react-icons/ai'
@@ -43,100 +43,100 @@ import { useFormspark } from '@formspark/use-formspark'
 import { useState } from 'react'
 
 const Contact = () => {
-  const [submit, submitting] = useFormspark({
-    formId: process.env.FORMSPARK_FORM_ID,
-  })
+	const [submit, submitting] = useFormspark({
+		formId: process.env.FORMSPARK_FORM_ID,
+	})
 
-  const [message, setMessage] = useState('')
+	const [message, setMessage] = useState('')
 
-  const onSubmit = async (e) => {
-    e.preventDefault()
-    await submit({ message })
-    alert('Form submitted')
-  }
-  return (
-    <Section id='contact'>
-      <Margin></Margin>
+	const onSubmit = async (e) => {
+		e.preventDefault()
+		await submit({ message })
+		alert('Form submitted')
+	}
+	return (
+		<Section id='contact'>
+			<Margin></Margin>
 
-      <SectionTitle>
-        <Title>Contact</Title>
-      </SectionTitle>
-      <Container>
-        <LeftSection>
-          <LeftContainer>
-            <ContentTitle>Contact Info</ContentTitle>
-            <InfoContainer>
-              <TopDiv>
-                <MailDiv>
-                  <RiMailSendLine size='4rem' />
-                </MailDiv>
-              </TopDiv>
-              <InfoDiv>
-                <Info>
-                  <AiOutlineMail size='1.2rem' />
-                  <Span> ludahmaxs@gmail.com</Span>
-                </Info>
-                <Info>
-                  <FiPhoneIncoming size='1.2rem' />
-                  <Span> +919986628817</Span>
-                </Info>
-              </InfoDiv>
+			<SectionTitle>
+				<Title>Contact</Title>
+			</SectionTitle>
+			<Container>
+				<LeftSection>
+					<LeftContainer>
+						<ContentTitle>Contact Info</ContentTitle>
+						<InfoContainer>
+							<TopDiv>
+								<MailDiv>
+									<RiMailSendLine size='4rem' />
+								</MailDiv>
+							</TopDiv>
+							<InfoDiv>
+								<Info>
+									<AiOutlineMail size='1.2rem' />
+									<Span> ludahmaxs@gmail.com</Span>
+								</Info>
+								<Info>
+									<FiPhoneIncoming size='1.2rem' />
+									<Span> +254114907009</Span>
+								</Info>
+							</InfoDiv>
 
-              <SocialSection>
-                <Line1></Line1>
-                <SocialDiv>
-                  <SocialIcon href='https://www.linkedin.com/in/lawrence-makori-230bb348/'>
-                    <AiFillLinkedin size='1.5rem' />
-                  </SocialIcon>
-                </SocialDiv>
-                <Line2></Line2>
-              </SocialSection>
-            </InfoContainer>
-          </LeftContainer>
-        </LeftSection>
-        <RightSection>
-          <RightContainer>
-            <ContentTitle>Contact Me</ContentTitle>
-            <ContactContainer>
-              <Form action='https://submit-form.com/XS1q10g4'>
-                <ContentDiv>
-                  <LabelDiv>
-                    <Label htmlFor='name'>Name</Label>
-                  </LabelDiv>
-                  <Input type='hidden' name='_feedback.dark' value='true' />
+							<SocialSection>
+								<Line1></Line1>
+								<SocialDiv>
+									<SocialIcon href='https://www.linkedin.com/in/lawrence-makori-230bb348/'>
+										<AiFillLinkedin size='1.5rem' />
+									</SocialIcon>
+								</SocialDiv>
+								<Line2></Line2>
+							</SocialSection>
+						</InfoContainer>
+					</LeftContainer>
+				</LeftSection>
+				<RightSection>
+					<RightContainer>
+						<ContentTitle>Contact Me</ContentTitle>
+						<ContactContainer>
+							<Form action='https://submit-form.com/XS1q10g4'>
+								<ContentDiv>
+									<LabelDiv>
+										<Label htmlFor='name'>Name</Label>
+									</LabelDiv>
+									<Input type='hidden' name='_feedback.dark' value='true' />
 
-                  <Input type='text' name='name'></Input>
-                </ContentDiv>
-                <ContentDiv>
-                  <LabelDiv>
-                    <Label htmlFor='email'>E-Mail</Label>
-                  </LabelDiv>
-                  <Input type='email' name='email' required={true}></Input>
-                </ContentDiv>
-                <ContentDiv>
-                  <LabelDiv>
-                    <Label htmlFor='message'>Your Message</Label>
-                  </LabelDiv>
+									<Input type='text' name='name'></Input>
+								</ContentDiv>
+								<ContentDiv>
+									<LabelDiv>
+										<Label htmlFor='email'>E-Mail</Label>
+									</LabelDiv>
+									<Input type='email' name='email' required={true}></Input>
+								</ContentDiv>
+								<ContentDiv>
+									<LabelDiv>
+										<Label htmlFor='message'>Your Message</Label>
+									</LabelDiv>
 
-                  <TextArea
-                    type='text'
-                    name='message'
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                  ></TextArea>
-                </ContentDiv>
-                <ContentDiv>
-                  <Button type='submit' disabled={submitting}>
-                    Send
-                  </Button>
-                </ContentDiv>
-              </Form>
-            </ContactContainer>
-          </RightContainer>
-        </RightSection>
-      </Container>
-      <style>
-        {`
+									<TextArea
+										type='text'
+										name='message'
+										value={message}
+										onChange={(e) => setMessage(e.target.value)}
+									></TextArea>
+								</ContentDiv>
+								<ContentDiv>
+									<Button type='submit' disabled={submitting}>
+										Send
+									</Button>
+								</ContentDiv>
+							</Form>
+						</ContactContainer>
+					</RightContainer>
+				</RightSection>
+			</Container>
+			<style>
+				{`
           input:-webkit-autofill,
           input:-webkit-autofill:hover, 
           input:-webkit-autofill:focus, 
@@ -145,9 +145,9 @@ const Contact = () => {
             -webkit-box-shadow: 0 20px 10px 10px black inset !important;
           }
         `}
-      </style>
-    </Section>
-  )
+			</style>
+		</Section>
+	)
 }
 
 export default Contact
--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -1,78 +1,79 @@
 import { AiFillGithub, AiOutlineCloudServer } from 'react-icons/ai'
 import {
-  ProjectsContainer,
-  Card,
-  ImageContainer,
-  Img,
-  ContentContainer,
-  ProjectTitle,
-  ProjectDesc,
-  SocialsContainer,
-  SocialsDiv,
-  CardButton,
-  Stack,
-  StackCard,
-  P,
-  TitleDiv,
-  DescDiv,
-  BottomDiv,
+	ProjectsContainer,
+	Card,
+	ImageContainer,
+	Img,
+	ContentContainer,
+	ProjectTitle,
+	ProjectDesc,
+	SocialsContainer,
+	SocialsDiv,
+	CardButton,
+	Stack,
+	StackCard,
+	P,
+	TitleDiv,
+	DescDiv,
+	BottomDiv,
 } from '../styles/components/projectCards'
 import myProjects from '../constants/projects'
 import {
-  AiFillCloseCircle,
-  AiOutlineCloseCircle,
-  IoIosCloseCircleOutline,
+	AiFillCloseCircle,
+	AiOutlineCloseCircle,
+	IoIosCloseCircleOutline,
 } from 'react-icons/io'
 
 const ProjectsCard = ({ currentId, state }) => {
-  const display = myProjects.filter((project) => project.id === currentId)
-  const { id, img, title, tech, despription, git, cloud } = display[0]
-  return (
-    <ProjectsContainer>
-      <Card id={id}>
-        <ImageContainer>
-          <Img src={img} alt='Projects'></Img>
-        </ImageContainer>
-        <ContentContainer>
-          <TitleDiv>
-            <ProjectTitle>{title}</ProjectTitle>
-          </TitleDiv>
-          <StackCard>
-            {tech.map((item, i) => (
-              <Stack key={i}>
-                <P>{item}</P>
-              </Stack>
-            ))}
-          </StackCard>
-          <DescDiv>
-            <ProjectDesc>{despription}</ProjectDesc>
-          </DescDiv>
-          <BottomDiv>
-            <SocialsContainer>
-              <SocialsDiv>
-                <a href={git}>
-                  <AiFillGithub size='1.8rem' />
-                </a>
-              </SocialsDiv>
-              <SocialsDiv>
-                <a href={cloud}>
-                  <AiOutlineCloudServer size='1.8rem' />
-                </a>
-              </SocialsDiv>
-            </SocialsContainer>
-          </BottomDiv>
-        </ContentContainer>
-        <CardButton>
-          <IoIosCloseCircleOutline
-            size='2.2rem'
-            onClick={() => {
-              state(false)
-            }}
-          />
-        </CardButton>
-      </Card>
-    </ProjectsContainer>
-  )
+	const display = myProjects.filter((project) => project.id === currentId)
+	const { id, img, title, tech, despription, git, cloud } = display[0]
+
+	return (
+		<ProjectsContainer>
+			<Card id={id}>
+				<ImageContainer>
+					<Img src={img} alt='Projects'></Img>
+				</ImageContainer>
+				<ContentContainer>
+					<TitleDiv>
+						<ProjectTitle>{title}</ProjectTitle>
+					</TitleDiv>
+					<StackCard>
+						{tech.map((item, i) => (
+							<Stack key={i}>
+								<P>{item}</P>
+							</Stack>
+						))}
+					</StackCard>
+					<DescDiv>
+						<ProjectDesc>{despription}</ProjectDesc>
+					</DescDiv>
+					<BottomDiv>
+						<SocialsContainer>
+							<SocialsDiv>
+								<a href={git} target='_blank' rel='noopener noreferrer'>
+									<AiFillGithub size='1.8rem' />
+								</a>
+							</SocialsDiv>
+							<SocialsDiv>
+								<a href={cloud} target='_blank' rel='noopener noreferrer'>
+									<AiOutlineCloudServer size='1.8rem' />
+								</a>
+							</SocialsDiv>
+						</SocialsContainer>
+					</BottomDiv>
+				</ContentContainer>
+				<CardButton>
+					<IoIosCloseCircleOutline
+						size='2.2rem'
+						onClick={() => {
+							state(false)
+						}}
+					/>
+				</CardButton>
+			</Card>
+		</ProjectsContainer>
+	)
 }
 
 export default ProjectsCard
